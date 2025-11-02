d6e7d65d170a6cf2247ce8b2e5ac6e7fac7c12f8
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-11-02 17:55:38 +0100

    first commit

---
+++ part1/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a + b)
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
