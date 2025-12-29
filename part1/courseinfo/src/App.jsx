c1fa81da1e595e792fda561a064fad1b4e55570b
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-12-29 06:31:10 +0100

    Finished working on component state and event handlers

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,9 +1,43 @@
 import { useState } from 'react'
 
-const App = (props) => {
-  const {counter} = props
+const Display = ({counter}) => <div> {counter} </div>
+  
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  console.log('increasing, value before', counter)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+  console.log('decreasing, value before', counter)
+
+  const setToZero = () => setCounter(0)
+  console.log('resetting to zero, value before', counter)
+  
+ 
+
   return (
-    <div>{counter}</div>
+    <div>
+        <Display 
+          counter={counter}/>
+        <Button 
+          onClick={increaseByOne}
+          text = 'plus'
+        />
+
+        <Button 
+          onClick={setToZero}
+          text = 'zero'
+        />
+
+        <Button 
+          onClick={decreaseByOne}
+          text = 'minus'
+        />
+    </div>
   )
 }
 
