d9fb423338dea919cbdd87a5c54e4bec618c16ca
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2026-01-08 05:54:04 +0100

    created array with a memory of clicking events

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,44 +1,36 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div> {counter} </div>
-  
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
-  console.log('increasing, value before', counter)
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        console.log('left before', left)
+        setLeft(left + 1)
+        console.log('left after', left)
+        setTotal(left + right)
+    }
 
-  const decreaseByOne = () => setCounter(counter - 1)
-  console.log('decreasing, value before', counter)
+const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
+}
 
-  const setToZero = () => setCounter(0)
-  console.log('resetting to zero, value before', counter)
-  
- 
-
-  return (
+return (
     <div>
-        <Display 
-          counter={counter}/>
-        <Button 
-          onClick={increaseByOne}
-          text = 'plus'
-        />
-
-        <Button 
-          onClick={setToZero}
-          text = 'zero'
-        />
-
-        <Button 
-          onClick={decreaseByOne}
-          text = 'minus'
-        />
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+        <p>{allClicks.join(' ')}</p>
+        <p>total {total}</p>
     </div>
-  )
+)
 }
 
 export default App
---
+++ part1/courseinfo/src/App_counter.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const Display = ({counter}) => <div> {counter} </div>
+  
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  console.log('increasing, value before', counter)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+  console.log('decreasing, value before', counter)
+
+  const setToZero = () => setCounter(0)
+  console.log('resetting to zero, value before', counter)
+  
+ 
+
+  return (
+    <div>
+        <Display 
+          counter={counter}/>
+        <Button 
+          onClick={increaseByOne}
+          text = 'plus'
+        />
+
+        <Button 
+          onClick={setToZero}
+          text = 'zero'
+        />
+
+        <Button 
+          onClick={decreaseByOne}
+          text = 'minus'
+        />
+    </div>
+  )
+}
+
+export default App
