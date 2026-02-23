3689c6ec5a84b910b56f1f3e904c2e21095fb0bf
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2026-02-23 06:50:31 +0100

    finished exercise 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,23 +1,36 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
-    </div>
-  )
-}
-
+import { useState } from 'react'
 
 const App = () => {
-  
-  const friends = [
-    {name: "Peter", age: 19},
-    {name: "Maya", age: 22}
-  ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-        <p>{friends[0].name} {friends[0].age}</p>
-        <p>{friends[1].name} {friends[1].age}</p>
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+
+      <h1>statistics</h1>
+      good: {good} <br></br>
+
+      neutral: {neutral} <br></br>
+
+      bad: {bad} <br></br>
     </div>
   )
 }
