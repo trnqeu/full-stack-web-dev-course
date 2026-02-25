2d731150d527d3ac356ac778c861b78014e54a20
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2026-02-25 06:43:24 +0100

    finished 1.11*: unicafe step 6, rendered Statistics into an html table

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,7 +5,10 @@ const Button = ({ handleClick, text }) => {
 }
 
 const StatisticLine = ({ text, value }) => {
-  return <p> {text} {value}</p>
+  return (<tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>)
 }
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -18,12 +21,16 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
       <div>
         <h1>statistics</h1>
-        <StatisticLine text='good' value={good} />
-        <StatisticLine text='neutral' value={neutral} />
-        <StatisticLine text='bad' value={bad} />
-        <StatisticLine text='all' value={total} />
-        <StatisticLine text='average' value={average} />
-        <StatisticLine text='positive' value={positive + "%"} />
+        <table>
+          <tbody>
+            <StatisticLine text='good' value={good} />
+            <StatisticLine text='neutral' value={neutral} />
+            <StatisticLine text='bad' value={bad} />
+            <StatisticLine text='all' value={total} />
+            <StatisticLine text='average' value={average} />
+            <StatisticLine text='positive' value={positive + "%"} />
+          </tbody>
+        </table>
 
       </div>
     )
