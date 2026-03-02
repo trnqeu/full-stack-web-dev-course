741f1abe0757d7efd7c5b4f90bfdf372be2f1905
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2026-03-02 07:00:33 +0100

    Destructured Note component

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,14 +1,17 @@
 const App = (props) => {
-  const { notes } = props
+  const Note = ({ note }) => {
+    return (
+      <li>note.content</li>
+    )
+  }
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note =>
-          <li key={note.id}>
-            {note.content}
-          </li>)}
+          <Note key={note.id} note={note} />
+        )}
       </ul>
     </div>
   )
